5eb3ff7f4f8e0b68f87e9a704698cb136a411810
Kishen97 <43293778+Kishen97@users.noreply.github.com>
2018-09-18 01:24:44 +0200

    Update interrupts.py
    initialization of SPI for communication to the ADC

--- interrupts.py
+++ interrupts.py
@@ -9,3 +9,10 @@ def init_event_detect():
     	GPIO.add_event_detect(26,GPIO.FALLING,callback=frequency,bouncetime=300)
     	GPIO.add_event_detect(16,GPIO.FALLING,callback=stop, bouncetime=300)
 	GPIO.add_event_detect(20,GPIO.FALLING,callback=display,bouncetime=300)
+
+#initialization of SPI for communication to the ADC
+def init_spi(SPIMOSI, SPIMISO, SPICLK,SPICS):
+	GPIO.setup(SPIMOSI, GPIO.OUT)
+	GPIO.setup(SPIMISO, GPIO.IN)
+	GPIO.setup(SPICLK, GPIO.OUT)
+	GPIO.setup(SPICS, GPIO.OUT)
