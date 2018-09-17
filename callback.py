fd43b6aac3ab78e24bbf5b3ac356f7fdd94272de
Aadam97 <43293815+Aadam97@users.noreply.github.com>
2018-09-18 01:05:50 +0200

    Added the required global variable
    Added the required global variable for the various callback function

--- callback.py
+++ callback.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+
+#Global Variables
+time_array = []
+timer_array = []
+pot_array = []
+tempre_array =[]
+light_array = []
+values =[0]*8
+stop_start = True
+sleeptime=0.5
+timer=0
