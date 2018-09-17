5ba98f47d7c1365471d65e0b38eafe2bdc6d0835
Aadam97 <43293815+Aadam97@users.noreply.github.com>
2018-09-18 01:09:06 +0200

    Creation of the Reset Callback Event
    Callback event for when the reset button has been created, in which resets the timer, sleep time and stop_start to default values. Along with clearing the terminal window.

--- callback.py
+++ callback.py
@@ -10,3 +10,17 @@ values =[0]*8
 stop_start = True
 sleeptime=0.5
 timer=0
+
+#when reset button is pressed, the timer,sleeptime and stop_start values are reset to default values
+#and the command line is cleared
+def reset(channel):
+	global timer
+	global sleeptime
+	global stop_start
+	timer =0.0
+	sleeptime=0.5
+	stop_start=True
+	ter=sp.call('clear' ,shell=True)
+	print('{:9} {:7} {:6} {:6} {:5}'.format("Time", "Timer", "Pot","Temp","Light"))
+	time.sleep(1)
+
