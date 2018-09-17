5bedb81516b5e5f5f61a4f7748505aa37eb486dd
Kishen97 <43293778+Kishen97@users.noreply.github.com>
2018-09-18 01:12:53 +0200

    Event detection with callbacks
    Initialization of the event detection function which callbacks the various functions such as reset frequency stop and display

--- interrupts.py
+++ interrupts.py
@@ -3,3 +3,9 @@ def init_pushbuttons():
 	GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	
+def init_event_detect():
+	GPIO.add_event_detect(19,GPIO.FALLING,callback=reset ,bouncetime=300)
+    	GPIO.add_event_detect(26,GPIO.FALLING,callback=frequency,bouncetime=300)
+    	GPIO.add_event_detect(16,GPIO.FALLING,callback=stop, bouncetime=300)
+	GPIO.add_event_detect(20,GPIO.FALLING,callback=display,bouncetime=300)
